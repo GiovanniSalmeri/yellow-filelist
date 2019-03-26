11dab43e44c6cc13046e57c73a8a471ef33f26aa
Giovanni <g.salmeri@mondodomani.org>
2019-03-26 21:27:11 +0100

    0.8.3

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# yellow-filelist
-List of files for download
+# Filelist 0.8.3
+
+Show a list of files for download.
+
+![Screenshot](filelist-screenshot.png?raw=true)
+
+## How to install extension
+
+1. [Download and install Datenstrom Yellow](https://github.com/datenstrom/yellow/).
+2. [Download extension](../../archive/master.zip). If you are using Safari, right click and select 'Download file as'.
+3. Copy `filelist.zip` into your `system/extensions` folder.
+
+To uninstall delete the [extension files](extension.ini).
+
+## How to add a list of files
+
+Create a [filelist] shortcut.
+
+The following arguments are available, all but the first argument are optional:
+
+`Folder` (default: `/`) = folder under `media/filelist` of files and subfolders to show  
+`Extensions` (default: `*`) = extensions of files to list, \* for all extensions; wrap multiple extensions into quotes   
+`Collapse` (default: `1`) = collapse the lists of subfolders  
+
+## How names of files and folders are shown
+
+Trailing digits and hyphens in the names of files and folders are used for sorting, but not shown in the link text.
+
+To display characters forbidden in filenames, use the following mnemonics prefixed with `=`:
+
+| Character | Name | Mnemonic |
+|---|---|---|
+| : | **c**olon | =c |
+| ? | **q**uestion mark | =q |
+| / | **s**lash | =s |
+| * | **a**sterisk | =a |
+| " | **d**ouble quotes | =d |
+| < | **l**ess than | =l |
+| > | **g**reater than | =g |
+| \\ | **b**ackslash | =b |
+| \| | **p**ipe | =p |
+| = | equal | == |
+
+E.g. the filename `5-Quo vadis=q.pdf` will be displayed as **Quo vadis?**.
+
+## Settings
+
+The following settings can be configured in file `system/settings/system.ini`:
+
+`filelistDir` (default = `media/filelist/`) = base directory for files   
+`filelistLocation` (default = `/media/filelist/`) = base location for files   
+`filelistEncode` (default = `mnemo`) = use `mnemo`nics or [`percent` encoding](https://en.wikipedia.org/wiki/Percent-encoding) for forbidden characters   
+`filelistCollapse` (default = `1`) = collapse the view of subfolders   
+`filelistShowType` (default = `0`) = show the type of each file   
+
+## Example
+
+Embedding a list of all files in the base folder:
+
+`[filelist]`
+
+Embedding a list of files in the subfolder `docs` with various options:
+
+`[filelist docs]`   
+`[filelist docs pdf]`   
+`[filelist docs "pdf odt"]`   
+`[filelist docs * 0]`   
+
+## Developer
+
+Giovanni Salmeri.
---
+++ filelist.php
@@ -0,0 +1,102 @@
+<?php
+// Filelist extension
+// Copyright (c) 2019 Giovanni Salmeri
+// This file may be used and distributed under the terms of the public license.
+
+class YellowFilelist {
+    const VERSION = "0.8.3";
+    const TYPE = "feature";
+    public $yellow;         //access to API
+    public $output;
+
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+        $this->yellow->system->setDefault("filelistDir", "media/filelist/");
+        $this->yellow->system->setDefault("filelistLocation", "/media/filelist/");
+        $this->yellow->system->setDefault("filelistEncode", "custom");
+        $this->yellow->system->setDefault("filelistCollapse", "1");
+        $this->yellow->system->setDefault("filelistShowType", "0");
+    }
+    
+    // Handle page content of shortcut
+    public function onParseContentShortcut($page, $name, $text, $type) {
+        $this->output = null;
+        if ($name=="filelist" && ($type=="block" || $type=="inline")) {
+            list($filePath, $fileExts, $collapse) = $this->yellow->toolbox->getTextArgs($text);
+            $exts = preg_split("/[\s,]+/", $exts, 0, PREG_SPLIT_NO_EMPTY);
+            if (empty($collapse)) $collapse = $this->yellow->system->get("filelistCollapse");
+            $fileLoc = $this->yellow->system->get("filelistLocation").$filePath;
+            $filePath = $this->yellow->system->get("filelistDir").$filePath;
+            if (is_dir($filePath)) {
+                $this->fileDir($filePath, $fileLoc, $exts, $collapse);
+            }
+        }
+        return $this->output;
+    }
+
+    function decodeFilename($name) {
+        if ($this->yellow->system->get("filelistEncode") == "percent") {
+            //%3D%3C%3E%3A%22%5C%2F%7C%3F%2A
+            //=<>:"\/|?*
+            return rawurldecode($name);
+        } else {
+            return preg_replace_callback("/=./", function($m) {
+                switch ($m[0]) {
+                    case "==": return "="; break;
+                    case "=l": return "<"; break;
+                    case "=g": return ">"; break;
+                    case "=c": return ":"; break;
+                    case "=d": return "\""; break;
+                    case "=s": return "/"; break;
+                    case "=b": return "\\"; break;
+                    case "=p": return "|"; break;
+                    case "=q": return "?"; break;
+                    case "=a": return "*"; break;
+                    default: return $m[0][1];
+                }
+            }, $name);
+        }
+    } 
+
+    function fileDir($startDir, $startLoc, $exts, $collapse) {
+        $dirHandle = opendir($startDir);
+        $files = $dirs = [];
+        while (($entry = readdir($dirHandle)) !== FALSE) {
+            if (is_file($startDir."/".$entry) && (!$exts || in_array($this->yellow->toolbox->getFileType($entry), $exts))) {
+                $files[] = $entry;
+            } elseif (is_dir($startDir."/".$entry) && ($entry[0] != ".")) {
+                $dirs[] = $entry;
+            }
+        }
+        closedir($dirHandle);
+        $this->output .= "<ul class=\"filelist".($collapse ? " collapsibleList" : "")."\">\n";
+        natcasesort($dirs);
+            foreach ($dirs as $dir) {
+            $desc = htmlspecialchars($this->decodeFilename((preg_replace("/^[\d\-]+/", "", $dir))));
+            $this->output .= "<li class=\"directory\">".$desc."\n";
+            $this->fileDir($startDir."/".$dir, $startLoc."/".$dir, $exts);
+            $this->output .= "</li>\n";
+        }
+        natcasesort($files);
+        foreach ($files as $file) {
+            $desc = htmlspecialchars($this->decodeFilename(pathinfo(preg_replace("/^[\d\-]+/", "", $file))["filename"]));
+            $link = implode('/', array_map('rawurlencode', explode('/', $startLoc. "/".$file)));
+            $this->output .= "<li class=\"file\"><a href=\"".$link."\">".$desc."</a>";
+            if ($this->yellow->system->get("filelistShowType")) $this->output .= " <span class=\"filetype\">".$this->yellow->toolbox->getFileType($entry)."</span>";
+            $this->output .= "</li>\n";
+        }
+        $this->output .= "</ul>\n";
+   }
+    
+    // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output = null;
+        if ($name == "header") {
+            $extensionLocation = $this->yellow->system->get("serverBase").$this->yellow->system->get("extensionLocation");
+            $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}filelist.css\" />\n";
+            $output .= "<script type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}filelist.js\"></script>\n";
+        }
+        return $output;
+    }
+}
