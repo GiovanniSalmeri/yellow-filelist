932dcab9fff698130ae139d97df6f5fd06a080a4
Giovanni <g.salmeri@mondodomani.org>
2019-03-26 22:06:42 +0100

    Fixed value of setting

--- filelist.php
+++ filelist.php
@@ -14,7 +14,7 @@ class YellowFilelist {
         $this->yellow = $yellow;
         $this->yellow->system->setDefault("filelistDir", "media/filelist/");
         $this->yellow->system->setDefault("filelistLocation", "/media/filelist/");
-        $this->yellow->system->setDefault("filelistEncode", "custom");
+        $this->yellow->system->setDefault("filelistEncode", "mnemo");
         $this->yellow->system->setDefault("filelistCollapse", "1");
         $this->yellow->system->setDefault("filelistShowType", "0");
     }
@@ -40,7 +40,7 @@ class YellowFilelist {
             //%3D%3C%3E%3A%22%5C%2F%7C%3F%2A
             //=<>:"\/|?*
             return rawurldecode($name);
-        } else {
+        } elseif ($this->yellow->system->get("filelistEncode") == "mnemo") {
             return preg_replace_callback("/=./", function($m) {
                 switch ($m[0]) {
                     case "==": return "="; break;
@@ -72,7 +72,7 @@ class YellowFilelist {
         closedir($dirHandle);
         $this->output .= "<ul class=\"filelist".($collapse ? " collapsibleList" : "")."\">\n";
         natcasesort($dirs);
-            foreach ($dirs as $dir) {
+        foreach ($dirs as $dir) {
             $desc = htmlspecialchars($this->decodeFilename((preg_replace("/^[\d\-]+/", "", $dir))));
             $this->output .= "<li class=\"directory\">".$desc."\n";
             $this->fileDir($startDir."/".$dir, $startLoc."/".$dir, $exts);
